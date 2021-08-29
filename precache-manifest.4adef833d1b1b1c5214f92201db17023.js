080c4f7d44c4326ce92298f35ba7e311e76cf6ad
Code Sandbox <hello@codesandbox.io>
2021-08-29 22:41:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4adef833d1b1b1c5214f92201db17023.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b249d1217cf01067d9c4c25a4526b07",
+    "url": "https://luigicleffi.github.io/csb-w804b/index.html"
+  },
+  {
+    "revision": "a5eb17269080b19e7594",
+    "url": "https://luigicleffi.github.io/csb-w804b/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "b0efd3fb700517accbb1",
+    "url": "https://luigicleffi.github.io/csb-w804b/static/js/2.186c7d2e.chunk.js"
+  },
+  {
+    "revision": "a5eb17269080b19e7594",
+    "url": "https://luigicleffi.github.io/csb-w804b/static/js/main.d8e543b2.chunk.js"
+  },
+  {
+    "revision": "00c9248ed491781cf558",
+    "url": "https://luigicleffi.github.io/csb-w804b/static/js/runtime~main.8edddc90.js"
+  }
+]);
